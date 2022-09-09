385de0c8d08b02dcb2df4906d4c004de3e9c579a
Alexander Kolmachikhin <akolmachikhin@icerockdev.com>
2022-09-09 23:49:37 +0700

    little save

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,15 +11,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'MaterialApp:title',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('AppBar:title'),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: RandomWords(),
         ),
       ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = generateWordPairs().take(100).toList();
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, i) {
+        return ListTile(
+          title: Text(
+            _suggestions[i].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+      itemCount: _suggestions.length,
+      physics: const BouncingScrollPhysics(),
     );
   }
 }
